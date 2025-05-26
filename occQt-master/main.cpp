93db7c986632d4a439bda22187a6448b67400434
WorthWhile1998 <131428536+WorthWhile1998@users.noreply.github.com>
2025-05-26 14:09:14 +0800

    Add files via upload

---
+++ occQt-master/main.cpp
@@ -0,0 +1,18 @@
+/*
+*   All Rights Reserved.
+*
+*/
+
+
+#include "occQt.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    occQt w;
+    w.show();
+
+    return a.exec();
+}
